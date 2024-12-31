c6a81a70bf7894850c8f17917e3d91868150e539
sisi-tarak <sisindrisingamsetti@gmail.com>
2024-12-31 23:44:43 +0530

    boiler plate required installations completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Container } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Container maxWidth="md">
+        <h1 className="text-3xl text-white font-bold">Webortex!!</h1>
+      </Container>
+    </>
+  );
+}
+
+export default App;
