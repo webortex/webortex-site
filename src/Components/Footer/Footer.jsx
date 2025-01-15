11c6581d4680de41e31bae62808797a6d7b73f44
sisi-tarak <sisindrisingamsetti@gmail.com>
2025-01-15 20:13:18 +0530

    footer responsive updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -53,12 +53,12 @@ const Footer = () => {
       </Container>
       <div className="w-full bg-[#0A0E10] py-8">
         <Container maxWidth="xl">
-          <div className="flex flex-col md:flex-row justify-center text-center md:justify-between w-full mb-8 md:items-start gap-y-4 md:gap-y-0">
+          <div className="flex flex-col md:flex-row justify-center text-center md:justify-between w-full mb-8 md:items-start gap-y-3 md:gap-y-0">
             <div className="flex flex-col justify-center text-center md:text-left w-full md:w-1/2 lg:w-1/3">
               <img
                 src={Webortexlogo2}
                 alt="Webortex Logo"
-                className="mb-4 mx-auto md:mx-0 md:ml-[30px]"
+                className="mb-3 md:mb-4 mx-auto md:mx-0 md:ml-[30px]"
                 style={{ width: "60%" }}
               />
               <p className="font-outlet text-center md:text-left mb-4 md:ml-12 md:text-left font-light text-[14px] md:text-[14px] md:text-[16px] leading-[26px]">
@@ -67,7 +67,7 @@ const Footer = () => {
               </p>
             </div>
             <div className="justify-items-center md:justify-items-start md:items-start md:mr-10 font-outlet font-light text-[14px] md:text-[16px] leading-[26px]">
-              <div className="items-center mb-4">
+              <div className="items-center mb-2 md:mb-4">
                 <Email className="md:mr-2" />
                 <a
                   href="mailto:contact@webortex.com"
@@ -82,7 +82,7 @@ const Footer = () => {
                   +91 8688281821
                 </a>
               </div>
-              <div className="flex space-x-4">
+              <div className="flex space-x-2 md:space-x-4">
                 <Facebook className="text-dimgray hover:text-navlinkHoverColor cursor-pointer transition-colors duration-300" />
                 <Instagram className="text-dimgray hover:text-navlinkHoverColor cursor-pointer transition-colors duration-300" />
                 <LinkedIn className="text-dimgray hover:text-navlinkHoverColor cursor-pointer transition-colors duration-300" />
@@ -91,7 +91,7 @@ const Footer = () => {
           </div>
           <div className="w-fulls border-t border-gray-500 mb-4 "></div>
           <div className="flex flex-col md:flex-row justify-between w-full items-center font-outlet font-normal text-[14px] md:text-[16px] leading-[26px] gap-y-4 md:gap-y-0 text-center mt-8 md:mt-0">
-            <div className="flex md:ml-8 flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-5">
+            <div className="flex md:ml-8 flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-5">
               <a
                 href="#"
                 className="hover:text-navlinkHoverColor cursor-pointer"
@@ -123,7 +123,7 @@ const Footer = () => {
                 Contact
               </a>
             </div>
-            <div className="text-center md:text-right mt-4 mr-5 md:mt-0">
+            <div className="text-center md:text-right mt-4 md:mr-5 md:mt-0">
               <p>webortex © 2025 - All Right Reserved</p>
             </div>
           </div>
