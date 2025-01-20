c3c51a35e3cf146d54d5274bfa40302c6ab87bad
sisi-tarak <sisindrisingamsetti@gmail.com>
2025-01-20 22:49:22 +0530

    quotation page separated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from "./Components/Navbar/Navbar";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
 import Recruit from "./Components/Recruit/Recruit";
-import Quotation from "./Components/Quotation/Quotation";
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
           <Route exact path="/" element={<Homepage />} />
           <Route exact path="/lets-talk" element={<Contact />} />
           <Route exact path="/recruiting" element={<Recruit />} />
-          <Route exact path="/quotation" element={<Quotation />} />
         </Routes>
         <Footer />
       </Router>
