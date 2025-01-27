dfdbd1b2ee853e37ac6c21cfacb65429eef7528c
sisi-tarak <sisindrisingamsetti@gmail.com>
2025-01-27 20:40:45 +0530

    pull from remote main

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -4,7 +4,8 @@ const testimonials = [
   {
     id: 1,
     logo: "Gymstory",
-    title: "Kornix Is The Best Digital Agency I Have Ever Seen! Highly Recommended!",
+    title:
+      "Kornix Is The Best Digital Agency I Have Ever Seen! Highly Recommended!",
     content:
       "I recently hired Ideapeel for a custom web development project and couldn't be happier with the results. The team was able to bring my unique ideas to life and create a website that truly stands out.",
     name: "Diana Loreza",
@@ -21,14 +22,13 @@ const TestimonialSlider = () => {
   };
 
   const handlePrev = () => {
-    setCurrent((prev) => (prev - 1 + testimonials.length) % testimonials.length);
+    setCurrent(
+      (prev) => (prev - 1 + testimonials.length) % testimonials.length
+    );
   };
 
   return (
-    
-    <div className=" min-h-screen flex flex-col items-center justify-center text-white">
-     
-    </div>
+    <div className="h-full flex flex-col items-center justify-center text-white"></div>
   );
 };
 
