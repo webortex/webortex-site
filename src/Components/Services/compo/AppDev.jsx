2b559340556f8d186a0d243721b3c620c41a0c46
sisi-tarak <sisindrisingamsetti@gmail.com>
2025-01-27 23:03:50 +0530

    services section border width updated

--- src/Components/Services/compo/AppDev.jsx
+++ src/Components/Services/compo/AppDev.jsx
@@ -24,7 +24,7 @@ const AppDev = () => {
 
   return (
     <div>
-       <div className=" p-8 flex items-center flex-col  text-white w-[310px] h-[390px] border-2 rounded-[22px] border-borColor  ">
+       <div className=" p-8 flex items-center flex-col  text-white w-[310px] h-[390px] border rounded-3xl border-borColor">
           <p className="text-2xl font-semibold">App Development</p>
           <p className="text-xs text-smallcolor font-medium mt-2 mb-5">We increase revenue and ensure sustainable long-term growth </p>
           {/* App development */}
--- src/Components/Services/compo/Seo.jsx
+++ src/Components/Services/compo/Seo.jsx
@@ -1,18 +1,19 @@
-import Loader from './loader'
+import Loader from "./loader";
 
 const Seo = () => {
   return (
     <div>
-      <div className=" p-8 flex items-center flex-col  text-white w-[310px] h-[390px] border-2 rounded-[22px] border-borColor  ">
-          <p className="text-2xl font-semibold">SEO</p>
-          <p className="text-xs text-smallcolor font-medium mt-2 mb-5">We increase revenue and ensure sustainable long-term growth </p>
-          {/* SEo */}
+      <div className=" p-8 flex items-center flex-col  text-white w-[310px] h-[390px] border rounded-3xl border-borColor  ">
+        <p className="text-2xl font-semibold">SEO</p>
+        <p className="text-xs text-smallcolor font-medium mt-2 mb-5">
+          We increase revenue and ensure sustainable long-term growth{" "}
+        </p>
+        {/* SEo */}
 
-           <Loader />
-
-        </div>
+        <Loader />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Seo
+export default Seo;
