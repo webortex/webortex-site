2f738ac3be279f2fdc83240da88dcd245a1f86ba
sisi-tarak <sisindrisingamsetti@gmail.com>
2025-02-11 19:30:56 +0530

    services page animations secreen size update

--- src/Components/Services/ServicePage.jsx
+++ src/Components/Services/ServicePage.jsx
@@ -12,20 +12,20 @@ const ServicePage = () => {
         const { services } = value;
         const service = services.find((service) => service.slug === slug);
         return (
-          <Container maxWidth="lg">
+          <div>
             {service ? (
               <div>
-                <div className="flex justify-center my-10 relative">
+                <div className="flex justify-center h-screen py-10 relative">
                   {service.frameImg ? (
                     <img
                       src={service.frameImg}
                       alt={service.title}
-                      className="w-full md:max-w-[80%] h-auto rounded-2xl opacity-40"
+                      className="h-full rounded-2xl opacity-40"
                     />
                   ) : (
                     <video
                       src={service.frameVideo}
-                      className="w-full md:max-w-[80%] h-auto rounded-2xl opacity-40"
+                      className="h-full rounded-2xl opacity-40"
                       autoPlay
                       loop
                       muted
@@ -44,46 +44,51 @@ const ServicePage = () => {
                     </div>
                   </div>
                 </div>
-
-                {service.banners &&
-                  service.banners.map((banner, index) => (
-                    <div
-                      key={index}
-                      className={`relative flex flex-col-reverse md:flex-row justify-center items-center w-full my-8 md:my-20 ${
-                        index % 2 === 0 ? "md:flex-row" : "md:flex-row-reverse"
-                      }`}
-                    >
-                      <div className="flex flex-col justify-center items-center align-middle text-center md:text-left px-3 xs:px-14 sm:px-24 md:px-10 2xl:px-20">
-                        <h2 className="text-2xl xs:text-3xl md:text-4xl lg:text-5xl text-white">
-                          {banner.title}
-                        </h2>
-                        <p className="text-xs md:text-sm text-secondaryTextColor mt-2 md:mt-8">
-                          {banner.description}
-                        </p>
+                <Container maxWidth="lg">
+                  {service.banners &&
+                    service.banners.map((banner, index) => (
+                      <div
+                        key={index}
+                        className={`relative flex flex-col-reverse md:flex-row justify-center items-center w-full my-8 md:my-20 ${
+                          index % 2 === 0
+                            ? "md:flex-row"
+                            : "md:flex-row-reverse"
+                        }`}
+                      >
+                        <div className="flex flex-col justify-center items-center align-middle text-center md:text-left px-3 xs:px-14 sm:px-24 md:px-10 2xl:px-20">
+                          <h2 className="text-2xl xs:text-3xl md:text-4xl lg:text-5xl text-white">
+                            {banner.title}
+                          </h2>
+                          <p className="text-xs md:text-sm text-secondaryTextColor mt-2 md:mt-8">
+                            {banner.description}
+                          </p>
+                        </div>
+                        <div className="px-3 xs:px-10 sm:px-20 md:px-10 2xl:px-20 mb-5 md:mb-0">
+                          <img src={banner.image} alt="" />
+                        </div>
                       </div>
-                      <div className="px-3 xs:px-10 sm:px-20 md:px-10 2xl:px-20 mb-5 md:mb-0">
-                        <img src={banner.image} alt="" />
-                      </div>
-                    </div>
-                  ))}
+                    ))}
+                </Container>
               </div>
             ) : (
-              <div className="flex flex-col justify-center items-center text-center mx-auto h-screen h-dvh">
-                <h1 className="text-white tracking-wider md:tracking-widest font-medium text-4xl md:text-5xl">
-                  Service Not Found
-                </h1>
-                <p className="mt-4 text-secondaryTextColor text-sm md:text-base px-7 xs:px-0">
-                  The requested service does not exist.{" "}
-                  <Link
-                    to="/"
-                    className="text-navlinkHoverColor/80 hover:text-navlinkHoverColor transition-all duration-300 ease-in-out"
-                  >
-                    Back to Home
-                  </Link>
-                </p>
-              </div>
+              <Container maxWidth="lg">
+                <div className="flex flex-col justify-center items-center text-center mx-auto h-screen h-dvh">
+                  <h1 className="text-white tracking-wider md:tracking-widest font-medium text-4xl md:text-5xl">
+                    Service Not Found
+                  </h1>
+                  <p className="mt-4 text-secondaryTextColor text-sm md:text-base px-7 xs:px-0">
+                    The requested service does not exist.{" "}
+                    <Link
+                      to="/"
+                      className="text-navlinkHoverColor/80 hover:text-navlinkHoverColor transition-all duration-300 ease-in-out"
+                    >
+                      Back to Home
+                    </Link>
+                  </p>
+                </div>
+              </Container>
             )}
-          </Container>
+          </div>
         );
       }}
     </Consumer>
