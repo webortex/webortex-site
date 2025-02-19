93d08895f3db9325a20c24f1eb8d02855509e21f
sisi-tarak <sisindrisingamsetti@gmail.com>
2025-02-19 21:58:43 +0530

    loader changed into spining loader with blue logo color

--- src/Components/Services/compo/loader.jsx
+++ src/Components/Services/compo/loader.jsx
@@ -1,26 +1,11 @@
-
 const Loader = () => {
   return (
-    <div>{/* deeni css index.css file lo undi  : mean normal css  */}
-       <div className='mb-14'></div>
-          <div id="wifi-loader">
-            <svg viewBox="0 0 86 86" className="circle-outer">
-              <circle r="40" cy="43" cx="43" className="back"></circle>
-              <circle r="40" cy="43" cx="43" className="front"></circle>
-              <circle r="40" cy="43" cx="43" className="new"></circle>
-            </svg>
-            <svg viewBox="0 0 60 60" className="circle-middle">
-              <circle r="27" cy="30" cx="30" className="back"></circle>
-              <circle r="27" cy="30" cx="30" className="front"></circle>
-            </svg>
-            <svg viewBox="0 0 34 34" className="circle-inner">
-              <circle r="14" cy="17" cx="17" className="back"></circle>
-              <circle r="14" cy="17" cx="17" className="front"></circle>
-            </svg>
-            <div data-text="Searching" className="text"></div>
-          </div>
+    <div>
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-logoBlueColor"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
