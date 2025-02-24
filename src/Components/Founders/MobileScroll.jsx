54496e1b67dfac3997eb2d78f340ca306898f5ac
sisi-tarak <sisindrisingamsetti@gmail.com>
2025-02-24 22:40:10 +0530

    founders section completed responsive on mobile

--- src/Components/Founders/MobileScroll.jsx
+++ src/Components/Founders/MobileScroll.jsx
@@ -54,11 +54,32 @@ const MobileScroll = () => {
                       <div className="bg-cover bg-no-repeat bg-center w-[100vw] h-full -z-1 bg-gray-100">
                         <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full h-[min(85vw,500px)] mt-[-8rem]">
                           <div className="flex flex-col h-full w-full">
-                            <div className="w-full relative">
-                              <p className="text-xs sm:text-sm md:text-base font-semibold text-gray-800 leading-relaxed mb-8 mt-2">
+                            <div className="flex flex-col relative mb-4">
+                              <p className="text-xs sm:text-sm md:text-base font-semibold text-gray-800 leading-relaxed mb-2 mt-2">
                                 # {founder.title}
                               </p>
-                              <div className="w-[min(65vw,1000px)] md:w-[min(50vw,1000px)] h-[min(65vw,1000px)] md:h-[min(50vw,1000px)] mx-auto">
+
+                              <div className="flex justify-center items-center relative w-full mb-3">
+                                <div className="absolute right-0 top-[50%] transform translate-y-[-50%] mt-[-50px]">
+                                  <div className="group duration-500 hover:-skew-x-0 skew-x-6 hover:translate-x-2">
+                                    <div className="group-hover:duration-400 relative rounded-xl w-20 h-12 xs:w-24 xs:h-14 md:w-28 md:h-16 bg-zinc-800 text-gray-50 flex flex-col justify-center items-center before:-skew-x-12 before:rounded-xl before:absolute before:content[''] before:bg-neutral-700 before:right-2 md:before:right-3 before:top-0 before:w-20 before:h-10 xs:before:w-24 xs:before:h-12 md:before:w-28 md:before:h-14 before:-z-10">
+                                      <span className="text-xs sm:text-sm font-medium xs:font-semibold">
+                                        Co
+                                      </span>
+                                      <p className="text-xs sm:text-sm font-medium xs:font-semibold">
+                                        Founder
+                                      </p>
+                                    </div>
+                                  </div>
+                                </div>
+                                <h2 className="order-1 text-[25px] xs:text-[30px] text-center font-bold text-gray-800 mt-6">
+                                  {founder.name}
+                                </h2>
+                              </div>
+                            </div>
+
+                            <div className="flex justify-center items-center">
+                              <div className="flex justify-center items-center w-[min(65vw,1000px)] md:w-[min(50vw,1000px)] h-[min(65vw,1000px)] md:h-[min(50vw,1000px)]">
                                 <img
                                   className="relative h-full rounded-2xl filter grayscale"
                                   src={founder.image}
@@ -67,25 +88,7 @@ const MobileScroll = () => {
                               </div>
                             </div>
 
-                            <div className="w-full flex flex-col justify-center bg-gray-100 z-20 py-5 sm:pb-10">
-                              <div className="items-center">
-                                <div className="absolute right-0 top-0 transform translate-y-[-50%]">
-                                  <div className="group duration-500 hover:-skew-x-0 skew-x-6 hover:translate-x-2">
-                                    <div className="group-hover:duration-400 relative rounded-xl w-16 h-10 xs:w-24 xs:h-14 md:w-28 md:h-16 bg-zinc-800 text-gray-50 flex flex-col justify-center items-center before:-skew-x-12 before:rounded-xl before:absolute before:content[''] before:bg-neutral-700 before:right-2 md:before:right-3 before:top-0 before:w-16 before:h-8 xs:before:w-24 xs:before:h-12 md:before:w-28 md:before:h-14 before:-z-10">
-                                      <span className="text-xs sm:text-sm md:text-lg font-semibold">
-                                        Co
-                                      </span>
-                                      <p className="text-xs sm:text-sm md:text-lg font-semibold">
-                                        Founder
-                                      </p>
-                                    </div>
-                                  </div>
-                                </div>
-                                <h2 className="order-1 text-[22px] md:text-[30px] text-center font-bold text-gray-800 mb-2 mt-2">
-                                  {founder.name}
-                                </h2>
-                              </div>
-
+                            <div className="flex flex-col justify-center items-center bg-gray-100 z-20 py-5 mt-4">
                               <p className="text-xs sm:text-sm md:text-base text-center text-gray-600 px-[1%] sm:px-[3%] leading-relaxed mb-6">
                                 {founder.quote}
                               </p>
@@ -93,7 +96,7 @@ const MobileScroll = () => {
                                 <Button
                                   onClick={() =>
                                     window.open(
-                                      founder.src,
+                                      founder.url,
                                       "_blank",
                                       "noopener,noreferrer"
                                     )
